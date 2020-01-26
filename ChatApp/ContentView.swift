bb07723e010d552f974a4cf6a22ac7732a880870
riku yasui <oh.or9.3a@icloud.com>
2020-01-26 15:37:56 +0900

    Initial Commit

---
+++ ChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatApp
+//
+//  Created by riku yasui on 2020/01/26.
+//  Copyright © 2020 riku yasui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
